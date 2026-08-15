11f93aa37cc2c5d75d0f94a10c647cecd6aff7ad
Sathvik733 <Sathvik733@gmail.com>
2026-08-15 15:54:11 +0200

    Initialize Streamforge project structure

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from src.utils.logger import get_logger
+
+
+logger = get_logger(__name__)
+
+
+def main() -> None:
+    logger.info("Starting StreamForge")
+
+
+if __name__ == "__main__":
+    main()
